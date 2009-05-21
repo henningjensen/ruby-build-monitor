e3623dd1d469cdc1353646efab910f2868f5ea69
Henning Jensen <henning.jensen@gmail.com>
2009-05-22 00:15:07 +0200

    Refactoring and added basic support for hudson. Currently no authentication support.

--- hudson_monitor.rb
+++ hudson_monitor.rb
@@ -4,96 +4,65 @@ require 'open-uri'
 require 'build'
 
 class HudsonMonitor 
-  attr_accessor :debug
-  def initialize(properties)
-		@debug=true
-    @properties = properties
-  end
-  
-  def properties
-    ["hostname","username","password","job"] 
+  def initialize(hostname, job, username=nil, password=nil)
+		@notifier = Notifier.new
+		configure(hostname, job, username, password)
   end
 
-  def print_config
-    @properties.keys.each do |p|
-      puts p + "=>" + @properties[p]
-      end
-  end
+	def configure(hostname, job, username, password)
+		@hostname = hostname
+		@job = job
+		@username = username
+		@password = password
+	end
 
 	def get_build_status()
-#		authenticate() if @auth.nil?
-    xmldoc = parse_xml("job/" + @properties["job"] + "/lastSuccessfulBuild/api/xml")
+    xmldoc = parse_xml("/job/" + @job + "/lastBuild/api/xml")
 		parse_build_status(xmldoc)
 	end
 
 	def parse_build_status(xmldoc)
-		build_state = xmldoc.at("result").inner_html
-		status = nil
-		summary = nil
-		if build_state =~ /SUCCESS/ 
-			status = Status::SUCCESS
-			summary = parse_summary_success(xmldoc)
-    elsif build_state =~ /Fail/
-			status = Status::FAILURE
-			summary = parse_summary_failure(xmldoc)
-    else
-      status = Status::UNKNOWN
-			summary = parse_summary_unknown(xmldoc)
+		build_state = xmldoc.at("result")
+		build = nil
+		if build_state
+			if build_state.inner_html =~ /SUCCESS/ 
+				build = Build.new(Status::SUCCESS, parse_summary_success(xmldoc))
+   		elsif build_state.inner_html =~ /FAILURE/
+				build = Build.new(Status::FAILURE, parse_summary_failure(xmldoc))
+    	else
+      	build = Build.new(Status::UNKNOWN, parse_summary_unknown(xmldoc))
+			end
+		else
+			building = xmldoc.at("building")
+			if building
+				build = Build.new(Status::UNKNOWN, @job + " is currently building")
+			else
+      	build = Build.new(Status::UNKNOWN, parse_summary_unknown(xmldoc))
+			end
 		end
-		Build.new(status, summary)
+		@notifier.notify(build)
 	end
 
 	def parse_project_link(xmldoc)
 		url = xmldoc.at("url").inner_html
-		"<a href=\"" + url + "\">" + @properties["job"] + "</a>"
+		"<a href=\"" + url + "\">" + @job + "</a>"
 	end
 
 	def parse_summary_success(xmldoc)
-		summary = parse_project_link(xmldoc) 
-		summary << " was successfully built in " << xmldoc.at("duration").inner_html 
-		summary
+		parse_project_link(xmldoc) << " was successfully built."
 	end
 
 	def parse_summary_failure(xmldoc)
-		summary = parse_project_link(xmldoc) 
-		summary << " build failed with " << xmldoc.at("failedtestcount").inner_html << " failed tests. " 
-		summary << "Reason: " << xmldoc.at("buildreason").inner_html << "."
-		summary
+		parse_project_link(xmldoc) << " build failed."
 	end
 
 	def parse_summary_unknown(xmldoc)
-		summary = parse_project_link(xmldoc) 
-		summary << " was not built due to an error. Build state is " << xmldoc.at("buildstate").inner_html << "." 
-		summary
+		parse_project_link(xmldoc) << " was not built due to an error."
 	end
 
   def parse_xml(url)
-    doc = Hpricot(open(@properties["hostname"] + url).read)
-    if @debug
-      puts "---debug---"
-      puts doc
-      puts "---debug---"
-    end
+    doc = Hpricot(open(@hostname + url).read)
+		puts doc
     doc
-  end
-
-  def get_identifiers
-    doc = parse_xml("listBuildNames.action?auth=" + @auth)
-    identifiers = Array.new
-    (doc/"build").each do |e|
-        identifiers << (e/"key").inner_html + "|" + (e/"name").inner_html
-    end
-    identifiers
-  end
-  
-  def authenticate
-    doc = parse_xml("login.action?username=" + @properties["username"] + "&password=" + @properties["password"])
-    if doc.at("error") 
-        raise "Authentication failure: " + doc.at("error").inner_html
-    end
-    @auth = doc.at("auth").inner_html
-    if debug
-      puts "auth: " + @auth
-    end
   end
 end
